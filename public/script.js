3c24b7efa702ad697df803f50cf00599227bc25d
Henriq_Fonseca <henrique12095@gmail.com>
2022-10-31 09:02:11 -0300

    poojs

--- public/script.js
+++ public/script.js
@@ -1,88 +1,127 @@
-class Produto{
-
-    constructor(){
-        this.id = 1;
-        this.arrayProdutos = [];
+class animal{
+    constructor(animal,idade,especie){
+        this.animal= animal //1=Cachorro, 2=Gato
+        this.idade= idade;
+        this.especie= especie; //1 labrador, 2 pitbull, 3 siames
     }
-    
-    salvar(){
-        let produto = this.lerDados();
-        if(this.validaCampos(produto)){
-            this.adicionar(produto);
+
+    info(){
+        if(this.animal == 1){
+
+            console.log("Animal: Cachorro" )
+        }else if(this.animal == 2){
+            console.log("Animal: Gato" )
         }
-        this.listaTabela();
-        this.cancelar();
-    }
+        console.log("Idade:" + this.idade)
 
-    listaTabela(){
-        let tbody = document.getElementById('tbody');
-        tbody.innerText = '';
+        if(this.especie == 1){
 
-        for (let i = 0; i < this.arrayProdutos.length; i++) {
-            let tr = tbody.insertRow();
-
-            let td_id = tr.insertCell();
-            let td_produto = tr.insertCell();
-            let td_valor = tr.insertCell();
-            let td_acoes = tr.insertCell();
-            
-            td_id.innerText = this.arrayProdutos[i].id;
-            td_produto.innerText = this.arrayProdutos[i].nomeProduto;
-            td_valor.innerText = this.arrayProdutos[i].preco;
-
-           
-            
-            let imgDelete = document.createElement('img');
-            imgDelete.src = 'img/delete.png';
-            imgDelete.setAttribute("onclick", "produto.deletar(" + this.arrayProdutos[i].id +")");
-            
-            td_acoes.appendChild(imgDelete);
+            console.log("Especie/raça: labrador" )
+        }else if(this.especie == 2){
+            console.log("Especie/raça: pitbull" )
+        }else{
+            console.log("Especie/raça: siames" )
         }
     }
 
-    adicionar(produto){
-        this.arrayProdutos.push(produto);
-        this.id++;
-    }
-    
-    lerDados(){
-        let produto = {}
-
-        produto.id = this.id;
-        produto.nomeProduto = document.getElementById('produto').value;
-        produto.preco = document.getElementById('preco').value;
-        return produto;
+    set setAnimal(a){
+        this.animal=a
     }
 
-    validaCampos(produto){
-        let msg = '';
-        if (produto.nomeProduto == '') {
-            msg += '-informe o nome do Produto \n'
-        }
-        if (produto.nomePreco == '') {
-            msg += '-informe o preco do Produto \n'
-        }
-        if (msg != '') {
-            alert(msg);
-            return false
-        }
-        return true 
-    }
-    cancelar(){
-        document.getElementById('produto').value = '';
-        document.getElementById('preco').value = '';
-    }
-    
-    deletar(id){
-        let tbody = document.getElementById('tbody');
 
-        for (let i = 0; i < this.arrayProdutos.length; i++) {
-            if (this.arrayProdutos[i].id == id) {
-                this.arrayProdutos.splice(i , 1);
-                tbody.deleteRow(i);
-            }
+    set setidade(i){
+        if(i<0 || i>30){
+            alert("Idade inválida")
+        }else{
+            this.idade=i
+
         }
+    }
+    get getIdade(){
+        return this.idade
+    }
+
+    set setEspecie(e){
+        this.especie = e
     }
 }
 
-var produto = new Produto();
+class cachorro extends animal{
+    constructor(nome){
+        super('','','')
+        this.nome = nome
+    }
+
+    info(){
+        super.info()
+        console.log("===========================")
+        console.log("                  ")
+    }
+    //definir nome do cachorro
+    set setNome(n){
+        this.nome=n
+    }
+
+    //capturar nome do cachorro
+    get getNome(){
+        return this.nome
+    }
+
+}
+// CACHORRO 1
+let dog1 = new cachorro()
+
+dog1.setEspecie = 1 // 1 = labrador
+dog1.setidade=13    //definir idade
+dog1.setNome= "toby" //definir nome
+console.log("Nome do Animal: " + dog1.getNome)
+dog1.info()
+
+// CACHORRO 2
+let dog2 = new cachorro()
+
+dog2.setEspecie = 2     //pitbull
+dog2.setidade=3         //idade
+dog2.setNome = "mel"    //nome
+console.log("Nome do Animal: " + dog2.getNome)
+dog2.info()
+
+
+class gato extends animal{
+    constructor(cor, nome){
+        super('','','')
+        this.cor = cor
+        this.nome = nome
+    }
+
+    set setCor(cor){
+        this.cor= cor
+    }
+
+    get getCor(){
+        return this.cor
+    }
+
+    set setNomecat(nome){
+        this.nome=nome
+    }
+
+    get getNomecat(){
+        return this.nome
+    }
+
+    info(){
+        console.log("Nome do Gato: " + this.getNomecat)
+        super.info()
+        console.log("=================")
+        console.log("                  ")
+    }
+}
+
+// GATO
+let cat1 = new gato()
+cat1.setNomecat = "felizardo"
+cat1.setAnimal=2    // Define q ele é um gato
+cat1.setidade=3     //idade do gato
+cat1.setEspecie=3   //siames
+cat1.info()
